6261cf4b13f4545dbed2d6148caa33ab24404b73
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-18 19:22:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 require "pry"
 def my_collect(array)
   i = 0
+  new_array = []
   while i < array.length
-   yield(array[i])
+   new_array << yield(array[i])
    i = i + 1
-
  end
- array
+ 
 
  end
 my_collect([1, 2, 3]) {|item| item + 1; binding.pry}
